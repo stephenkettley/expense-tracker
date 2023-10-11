6630db01301cabb9494830eee1aecb440917e62e
Stephen Kettley <kettleystephen@gmail.com>
2023-10-11 09:45:25 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
 import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />); // this is the only component rendered directly into the html page.
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
